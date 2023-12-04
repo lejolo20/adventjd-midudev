22905665295cd916ea968a6b27062190ddb9ffe5
Leonardo Lopez <96030802+lejolo20@users.noreply.github.com>
2023-12-03 20:57:10 -0400

    feat: second problem solved

--- Readme.md
+++ Readme.md
@@ -28,7 +28,7 @@ gifts: any, , , hint
 
 ########### Reto 2 ##########
 
-En el taller de Santa, los elfos tienen una lista de regalos que desean fabricar y un conjunto limitado de materiales.
+E n el taller de Santa, los elfos tienen una lista de regalos que desean fabricar y un conjunto limitado de materiales.
 
 Los regalos son cadenas de texto y los materiales son caracteres. Tu tarea es escribir una función que, dada una lista de regalos y los materiales disponibles, devuelva una lista de los regalos que se pueden fabricar.
 
--- main.js
+++ main.js
@@ -14,4 +14,54 @@ console.log(findFirstRepeated([1, 2, 3, 4])); */
 
 /* Reto 2 */
 
+function manufacture(gifts, materials) {
+  /* let newArr = gifts.map((item) => {
+    const splited = item.split("");
+    return splited;
+  });
 
+  let compareArr = newArr.map((item) => {
+    let individual = item.map((innerItem) => {
+      return materials.includes(innerItem);
+    });
+
+    if (individual.every((item) => item == true)) {
+      return item;
+    } else return null;
+  });
+
+  let arrayWithoutNulls = compareArr.filter((item) => {
+    if (item != null) {
+      return item;
+    }
+  });
+
+  let responseArr = arrayWithoutNulls.map((item) => {
+    return item.join("");
+  });
+
+  return responseArr; */
+
+  /* Optimizada con GPT */
+  let responseArr = gifts
+    .map((item) => {
+      const splited = item.split("");
+      let individual = splited.map((innerItem) =>
+        materials.includes(innerItem)
+      );
+
+      if (individual.every((item) => item)) {
+        return splited.join("");
+      } else {
+        return null;
+      }
+    })
+    .filter((item) => item !== null);
+
+  return responseArr;
+}
+
+const gifts = ["libro", "ps5"];
+const materials = "psli";
+
+console.log(manufacture(gifts, materials)); // ["tren", "oso"]
